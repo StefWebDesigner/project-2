c214998e83807ce9c45aff10171a9184fa56a97a
Stefan <67237726+StefWebDesigner@users.noreply.github.com>
2022-04-20 21:00:28 -0400

    readded all files

---
+++ backend/revademy-springboot/src/main/java/com/Revature/Revademy/RevademyApplication.java
@@ -0,0 +1,13 @@
+package com.Revature.Revademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevademyApplication.class, args);
+	}
+
+}
