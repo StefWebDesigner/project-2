dd076185b8bcf4adc03f7441eec49680ed46966e
Stefan <67237726+StefWebDesigner@users.noreply.github.com>
2022-04-21 23:25:01 -0400

    basic header made

--- frontend/Revademy/projectapp/src/components/home/Home.js
+++ frontend/Revademy/projectapp/src/components/home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import HomeMain from "./HomeMain";
 import NavbarMain from "../navbar/NavbarMain";
+import Header from "../navbar/Header";
 
 const Home = () => {
     return (
         <>
+            <Header/>
             <NavbarMain/>
             <HomeMain/>
         </>
--- frontend/Revademy/projectapp/src/components/navbar/Header.js
+++ frontend/Revademy/projectapp/src/components/navbar/Header.js
@@ -1,14 +1,34 @@
 import React from 'react';
+import {Col, Container, Navbar, Row} from "react-bootstrap";
+import {BsSearch} from "react-icons/bs";
+
 
 //THIS WILL BE THE HEADER
 
 const Header = () => {
     return (
-        <div>
-
+        <>
         {/*    contains, logo, searchbar, merch store,     */}
 
-        </div>
+            <Navbar bg="dark">
+                <Container>
+                    <Row>
+                        <Col>
+                            <h1 className="headerItem">Logo</h1>
+                        </Col>
+                        <Col>
+                            <h1 className="headerItem">MerchStore</h1>
+                        </Col>
+                        <Col className="d-flex flex-row-reverse">
+                            <h1 className="headerItem"><BsSearch/> </h1>
+                        </Col>
+                    </Row>
+                </Container>
+
+            </Navbar>
+
+
+        </>
     );
 };
 
