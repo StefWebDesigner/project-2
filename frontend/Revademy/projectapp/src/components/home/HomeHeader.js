b11aaf88afeb80476d9820ae232b5e5b585fa9ac
Stefan <67237726+StefWebDesigner@users.noreply.github.com>
2022-04-22 13:27:01 -0400

    added flags to the values (HomeHeader.js)

--- frontend/Revademy/projectapp/src/components/home/HomeHeader.js
+++ frontend/Revademy/projectapp/src/components/home/HomeHeader.js
@@ -1,7 +1,7 @@
 import React, {useContext} from 'react';
 import {FaSmileBeam} from "react-icons/fa";
 import DataStore from "../../dataStore/dataStore";
-import {Col, Container, Row} from "react-bootstrap";
+import {Card, Col, Container, Row} from "react-bootstrap";
 
 
 const HomeHeader = () => {
@@ -19,8 +19,37 @@ const HomeHeader = () => {
                     {  user ? <h2 className="home-header-user">Welcome {user} </h2> : ""}
                 </Row>
                 <Row>
-                    <p className="home-header-title-subtitle"><FaSmileBeam/> Where learning is FUN!<FaSmileBeam/></p>
+                    <h4 className="home-header-title-subtitle"><FaSmileBeam/> Learning so fun you don't even notice!<FaSmileBeam/></h4>
                 </Row>
+                <Row>
+
+                    <Col>
+                        <Card>
+                            <h5 className="home-header-values-1">Tailor English Classes</h5>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card>
+                            <h5 className="home-header-values-2">Dynamic English Learning</h5>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card>
+                            <h5 className="home-header-values-3">Fun & Interactive</h5>
+                        </Card>
+                    </Col>
+
+
+                </Row>
+
+
+
+
+
+
+                {/*<Row>*/}
+                {/*    <p className="home-header-title-subtitle"><FaSmileBeam/> Where learning is FUN!<FaSmileBeam/></p>*/}
+                {/*</Row>*/}
                 </Container>
             </section>
 
