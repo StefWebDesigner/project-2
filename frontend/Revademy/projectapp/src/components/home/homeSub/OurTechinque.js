48c70712bbc9bac87d5a0b026f86637f524e1789
Stefan <67237726+StefWebDesigner@users.noreply.github.com>
2022-04-23 23:00:16 -0400

    added animated color gradianet background for our techinal sectio

--- frontend/Revademy/projectapp/src/components/home/homeSub/OurTechinque.js
+++ frontend/Revademy/projectapp/src/components/home/homeSub/OurTechinque.js
@@ -6,9 +6,10 @@ const OurTechinque = () => {
         <>
 
             <section>
+                <Card.Header className="custom-backgound">
+                    <h2 className="techinque-title">What We Do :</h2>
 
-                <h2 className="techinque-title">What We Do :</h2>
-
+                </Card.Header>
                 <Card>
                     <Row>
                         <Card.Body>
@@ -19,21 +20,32 @@ const OurTechinque = () => {
                                 />
                                 <p className="tecnique-item">We are experts in working with distance learning</p>
                             </section>
-                            <section>
+                        </Card.Body>
+                        <Card.Body>
+
+                        <section>
                                 <img
                                 className="techinque-images"
                                 src="img/forEveryone.png"
                                 />
                                 <p className="tecnique-item">A learning program for all children </p>
                             </section>
-                            <section>
+                    </Card.Body>
+                    <Card.Body>
+
+                    <section>
                                 <img
                                     className="techinque-images"
                                     src="img/onlineCall.png"
                                 />
                                 <p className="tecnique-item">We make eLearning simple and easy</p>
                             </section>
-                        </Card.Body>
+                </Card.Body>
+                <Card.Footer className="custom-backgound">
+
+                </Card.Footer>
+
+            {/*</Card.Body>*/}
                 </Row>
                 </Card>
             </section>
