9690cedf341a784aacbf5cbcaf9b3c3b3cf8e618
Stefan <67237726+StefWebDesigner@users.noreply.github.com>
2022-04-23 21:57:05 -0400

    added our techinque section with mobile optomizatoin(ourtechnique.js)

--- frontend/Revademy/projectapp/src/components/home/homeSub/OurTechinque.js
+++ frontend/Revademy/projectapp/src/components/home/homeSub/OurTechinque.js
@@ -1,31 +1,41 @@
 import React from 'react';
-import {Card} from "react-bootstrap";
+import {Card, Row} from "react-bootstrap";
 
 const OurTechinque = () => {
     return (
         <>
 
             <section>
-                <Card>
-                    <Card.Body>
-                        <img/>
-                        <p>We are experts in working with distance learning</p>
-                    </Card.Body>
-                </Card>
-                <Card>
-                    <Card.Body>
-                        <img/>
-                        <p>A learning program for all children </p>
-                    </Card.Body>
-                </Card>
-                <Card>
-                    <Card.Body>
-                        <img/>
-                        <p>We make eLearning simple and easy</p>
-                    </Card.Body>
-                </Card>
 
+                <h2 className="techinque-title">What We Do :</h2>
 
+                <Card>
+                    <Row>
+                        <Card.Body>
+                            <section>
+                                <img
+                                    className="techinque-images"
+                                    src="img/distance-learning.png"
+                                />
+                                <p className="tecnique-item">We are experts in working with distance learning</p>
+                            </section>
+                            <section>
+                                <img
+                                className="techinque-images"
+                                src="img/forEveryone.png"
+                                />
+                                <p className="tecnique-item">A learning program for all children </p>
+                            </section>
+                            <section>
+                                <img
+                                    className="techinque-images"
+                                    src="img/onlineCall.png"
+                                />
+                                <p className="tecnique-item">We make eLearning simple and easy</p>
+                            </section>
+                        </Card.Body>
+                </Row>
+                </Card>
             </section>
 
 
