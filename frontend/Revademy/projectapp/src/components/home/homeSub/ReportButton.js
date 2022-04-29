e040f9e119c42b0b542eea19e0998193cd7bd039
Stefan <67237726+StefWebDesigner@users.noreply.github.com>
2022-04-29 14:15:11 -0400

    making a modal for report

--- frontend/Revademy/projectapp/src/components/home/homeSub/ReportButton.js
+++ frontend/Revademy/projectapp/src/components/home/homeSub/ReportButton.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import {Button} from "react-bootstrap";
-
+import React, {useState} from 'react';
+import {Button, Form, FormControl, FormGroup, FormLabel, Modal, ModalBody, ModalFooter} from "react-bootstrap";
+import useModal from "./reportSection/useModal";
 const ReportButton = () => {
 
 
@@ -8,13 +8,89 @@ const ReportButton = () => {
         <>
             <section>
                 <div className="button-report-container">
-                    <button className="button-report">
+                    <button
+                        className="button-report"
+                        data-toggle="modal"
+                        data-target="#ReportModal"
+                    >
                         <span>Report</span>
                     </button>
                 </div>
-
             </section>
 
+            <Modal
+                size="md"
+                centered
+                id="ReportModal"
+            >
+                <Form  onSubmit="">
+                    <Modal.Header>
+                        <h3> Report Issue : </h3>
+                    </Modal.Header>
+                    <Modal.Body>
+                        {/*ENTER SUBJECT TITLE*/}
+                        <Form.Group>
+                            <Form.Label>
+                                Enter issue subject:
+                            </Form.Label>
+                            <Form.Control
+                                type="text"
+                                name="bugTitle"
+                                value=""
+                                placeholder="Issue Subject"
+                                required
+                            />
+                        </Form.Group>
+                        {/*ENTER LOCATION SLECTION */}
+                        <Form.Group>
+                            <Form.Label>
+                                Select the location of the issue
+                            </Form.Label>
+                            <Form.Select
+                                // style={{border: error ? "2px solid red" : ""}}
+                                size="lg"
+                                name="locationTypes"
+                                // onChange={handleFormData("account")}
+                            >
+                                <option value="">Open this select menu</option>
+                                <option value="homepage">Homepage</option>
+                                <option value="activitypage">Activity Page</option>
+                                <option value="eventpage">Event Page</option>
+                                <option value="matchingsection">Matching Section</option>
+                                <option value="wordsection"> Word Section</option>
+                                <option value="learningportal">Learning Portal</option>
+                            </Form.Select>
+                            {/* ERROR SECTION    */}
+                            {/*{*/}
+                            {/*    error ? (*/}
+                            {/*        <Form.text> This is required</Form.text>*/}
+                            {/*    ) : ""*/}
+                            {/*}*/}
+                        </Form.Group>
+                        {/* REPORT ISSUE CONTENT*/}
+                        <Form.Group>
+                            <Form.Label>
+                                Describe the issue:
+                            </Form.Label>
+                            <Form.Control
+                                type="text"
+                                name="bugDescription"
+                                value=""
+                                placeholder="Issue Subject"
+                                required
+                            />
+                        </Form.Group>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <button
+                            type="submit"
+                            className="button-report"
+                        >
+                            Submit
+                        </button>
+                    </Modal.Footer>
+                </Form>
+            </Modal>
 
         </>
     );
