0f48a77ebaaff618a4ff6a1ee9fdc98d8919d4a9
Stefan <67237726+StefWebDesigner@users.noreply.github.com>
2022-04-29 12:43:55 -0400

    added report button

---
+++ frontend/Revademy/projectapp/src/components/home/homeSub/ReportButton.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Button} from "react-bootstrap";
+
+const ReportButton = () => {
+
+
+    return (
+        <>
+            <section>
+                <button className="button-report">
+                    <span>Report</span>
+                </button>
+
+            </section>
+
+
+        </>
+    );
+};
+
+export default ReportButton;
--- frontend/Revademy/projectapp/src/components/userRelated/register/RegisterFinal.js
+++ frontend/Revademy/projectapp/src/components/userRelated/register/RegisterFinal.js
@@ -1,71 +1,71 @@
-// import React from 'react';
-// import {useNavigate} from "react-router-dom"
-// import {Button, Card} from "react-bootstrap";
-//
-// const RegisterFinal = ({values, prevStep}) => {
-//
-//    const navigate = useNavigate();
-//
-//     const {
-//         firstName,
-//         lastName,
-//         account,
-//         age,
-//         email,
-//         password} = values;
-//
-//
-//     const registerUserFinalSubmit = (e) => {
-//         e.preventDefault();
-//
-//         //To navigate to login page
-//         navigate("/login")
-//     }
-//
-//     return (
-//         <>
-//             <section>
-//
-//
-//
-//                 <Card style={{ marginTop: 100, textAlign: "left" }}>
-//                     <Card.Body>
-//                         <p>
-//                             <strong> First Name : </strong> {JSON.stringify(firstName)}
-//                         </p>
-//                         <p>
-//                             <strong> Last Name : </strong> {lastName}
-//                         </p>
-//                         <p>
-//                             <strong> Account : </strong>
-//                             {account}
-//
-//                         </p>
-//                         <p>
-//                             <strong> Age : </strong>
-//                             {age}
-//
-//                         </p>
-//                         <p>
-//                             <strong> Email : </strong> {email}
-//                         </p>
-//                         <p>
-//                             <strong> Password : </strong> {password}
-//                         </p>
-//                         <div style={{ display: "flex", justifyContent: "space-around" }}>
-//                             <Button variant="primary" onClick={prevStep}>
-//                                 Previous
-//                             </Button>
-//
-//                             <Button variant="primary" type="submit" onClick={registerUserFinalSubmit}>
-//                                 Submit
-//                             </Button>
-//                         </div>
-//                     </Card.Body>
-//                 </Card>
-//             </section>
-//         </>
-//     );
-// };
-//
-// export default RegisterFinal;
+import React from 'react';
+import {useNavigate} from "react-router-dom"
+import {Button, Card} from "react-bootstrap";
+
+const RegisterFinal = ({values, prevStep}) => {
+
+   const navigate = useNavigate();
+
+    const {
+        firstName,
+        lastName,
+        account,
+        age,
+        email,
+        password} = values;
+
+
+    const registerUserFinalSubmit = (e) => {
+        e.preventDefault();
+
+        //To navigate to login page
+        navigate("/login")
+    }
+
+    return (
+        <>
+            <section>
+
+
+
+                <Card style={{ marginTop: 100, textAlign: "left" }}>
+                    <Card.Body>
+                        <p>
+                            <strong> First Name : </strong> {JSON.stringify(firstName)}
+                        </p>
+                        <p>
+                            <strong> Last Name : </strong> {lastName}
+                        </p>
+                        <p>
+                            <strong> Account : </strong>
+                            {account}
+
+                        </p>
+                        <p>
+                            <strong> Age : </strong>
+                            {age}
+
+                        </p>
+                        <p>
+                            <strong> Email : </strong> {email}
+                        </p>
+                        <p>
+                            <strong> Password : </strong> {password}
+                        </p>
+                        <div style={{ display: "flex", justifyContent: "space-around" }}>
+                            <Button variant="primary" onClick={prevStep}>
+                                Previous
+                            </Button>
+
+                            <Button variant="primary" type="submit" onClick={registerUserFinalSubmit}>
+                                Submit
+                            </Button>
+                        </div>
+                    </Card.Body>
+                </Card>
+            </section>
+        </>
+    );
+};
+
+export default RegisterFinal;
