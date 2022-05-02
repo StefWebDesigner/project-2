41eb91a9e1c24733877961b66aa6d811fab1ed08
Daun Kim <101144992+daunkim18@users.noreply.github.com>
2022-05-01 19:24:13 -0500

    Add files via upload

--- frontend/Revademy/projectapp/src/components/events/Events.js
+++ frontend/Revademy/projectapp/src/components/events/Events.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import NavbarMain from "../navbar/NavbarMain";
+<<<<<<< HEAD
+import {Link} from "react-router-dom";
+=======
 
+>>>>>>> main
 const Events = () => {
 
     return (
         <>
             <NavbarMain/>
             <h1>Events</h1>
+<<<<<<< HEAD
+            <Link to="/recalendar"></Link>
+=======
+>>>>>>> main
 
         </>
     );
 };
 
+<<<<<<< HEAD
 export default Events;
+=======
+export default Events;
+>>>>>>> main
