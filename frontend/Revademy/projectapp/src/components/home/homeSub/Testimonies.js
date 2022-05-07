38feefdf40ecde90ff238bd6f56d2f1d51758149
matt-kott <mkott3@bellsouth.net>
2022-05-07 18:38:52 -0400

    fixed testimonies

--- frontend/Revademy/projectapp/src/components/home/homeSub/Testimonies.js
+++ frontend/Revademy/projectapp/src/components/home/homeSub/Testimonies.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import {Card, Col, Container, Row} from "react-bootstrap";
 import Slider from "react-slick";
-<<<<<<< HEAD
-import {ImQuotesLeft, ImQuotesRight} from "react-icons/im";
-=======
 import { useTranslation } from 'react-i18next';
-
->>>>>>> melslot
 
 
 const Testimonies = () => {
@@ -39,11 +34,7 @@ const Testimonies = () => {
                         </Card.Header>
                         <Card.Body className="testimony-body">
                             <h3>John</h3>
-<<<<<<< HEAD
-                            <p><ImQuotesLeft/>My children loved the classes and meet new freinds too!<ImQuotesRight/></p>
-=======
                             <p>{t("mychildren")}</p>
->>>>>>> melslot
                         </Card.Body>
                         <Card.Footer className="testimony-backgound">
                         </Card.Footer>
@@ -57,11 +48,7 @@ const Testimonies = () => {
                         </Card.Header>
                         <Card.Body className="testimony-body">
                             <h3>Maria</h3>
-<<<<<<< HEAD
-                            <p><ImQuotesLeft/>My child was so excited learning in class we signed for their event too!<ImQuotesRight/></p>
-=======
                             <p>{t("mychild")}</p>
->>>>>>> melslot
                         </Card.Body>
                         <Card.Footer className="testimony-backgound">
                         </Card.Footer>
@@ -75,11 +62,7 @@ const Testimonies = () => {
                         </Card.Header>
                         <Card.Body className="testimony-body">
                             <h3>Mark</h3>
-<<<<<<< HEAD
-                            <p><ImQuotesLeft/>The teachers were so nice and engaging with the students. Great materailks<ImQuotesRight/></p>
-=======
                             <p>{t("theteachers")}</p>
->>>>>>> melslot
                         </Card.Body>
                         <Card.Footer className="testimony-backgound">
                         </Card.Footer>
@@ -92,11 +75,7 @@ const Testimonies = () => {
                         </Card.Header>
                         <Card.Body className="testimony-body">
                             <h3>Alejandra</h3>
-<<<<<<< HEAD
-                            <p><ImQuotesLeft/>Hands downs best on english platfrom ever! AKA not bias at ALL!!!<ImQuotesRight/></p>
-=======
                             <p>{t("handsdown")}</p>
->>>>>>> melslot
                         </Card.Body>
                         <Card.Footer className="testimony-backgound">
                         </Card.Footer>
