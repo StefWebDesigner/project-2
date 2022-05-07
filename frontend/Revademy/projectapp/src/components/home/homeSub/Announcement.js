083b6aea7859b5197a963c0ab2cc1d102d1f6c67
Stefan <67237726+StefWebDesigner@users.noreply.github.com>
2022-05-06 23:52:39 -0400

    mapped the announcement content

--- frontend/Revademy/projectapp/src/components/home/homeSub/Announcement.js
+++ frontend/Revademy/projectapp/src/components/home/homeSub/Announcement.js
@@ -1,11 +1,30 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useEffect} from 'react';
 import {Card, Carousel, Container} from "react-bootstrap";
 import {FaSmileBeam} from "react-icons/fa";
+import AdminHomeContentDisplay from "../../admin/adminSubgroups/adminHomeContentSubGroup/AdminHomeContentDisplay";
+import {useState} from "react";
+import axios from "axios";
 
 const Announcement = () => {
 
+    const [displayAnnouncements, setDisplayAnnouncements] = useState([]);
+
+    const displayAnnouncement = () => {
+        axios.get("http://localhost:8080/announcement/all")
+            .then(response => {
+                const reports = response.data
+                console.log(reports)
+                setDisplayAnnouncements(response.data)
+            })
+    }
+
+    useEffect(() => {
+        displayAnnouncement()
+    }, [])
+
     return (
         <>
+
             <section>
                     <Card>
 
@@ -13,16 +32,26 @@ const Announcement = () => {
                             <h2 className="announce-title">Announcement</h2>
                         </Card.Header>
                         <Card.Body>
-                        <h4 className="announce-content">
+                        <h4 className="announce-content">     </h4>
 
-                                {/****** TO ADD SOME SCALLING AND EXCITING HOVER EFFECT OVER SMILE FACE*******/}
-                                <Fragment>
-                                    <p><FaSmileBeam/>  New events added like a field trip the science museum</p>
-                                    <p><FaSmileBeam/>  5 Games added to the list to learning with a smile on your face</p>
-                                    <p><FaSmileBeam/>  This is just an outline...lots of styling to come</p>
-                                </Fragment>
+                            {
 
-                        </h4>
+                                    displayAnnouncements.map((news, index) => {
+                                        return (
+                                            <div
+                                                key={news.contentId}
+                                            >
+                                                <p>{news.contentBody}</p>
+
+                                            </div>
+                                        )
+
+                                    })
+
+                            }
+
+
+
                         </Card.Body>
                         <Card.Footer className="custom-backgound">
 
