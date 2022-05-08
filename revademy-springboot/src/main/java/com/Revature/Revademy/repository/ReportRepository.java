87cac807132bdb80590614b07e13ce42b78ba328
Stefan <67237726+StefWebDesigner@users.noreply.github.com>
2022-05-08 19:09:37 -0400

    created the report grpah fact http requests

--- revademy-springboot/src/main/java/com/Revature/Revademy/repository/ReportRepository.java
+++ revademy-springboot/src/main/java/com/Revature/Revademy/repository/ReportRepository.java
@@ -16,7 +16,42 @@ public interface ReportRepository extends JpaRepository<Report, Integer> {
     @Query(value = "SELECT COUNT(location_types) FROM report_model WHERE location_types = (SELECT MAX(location_types) FROM report_model)", nativeQuery = true)
     Integer CountForMostReportedLocation();
 
-//    @Query
-//    String mostReportedLocation();
+
+
+//    FOLLOWING SQL IS FOR THE REPORT FACT GRAPH
+
+
+    @Query(value = "SELECT COUNT(location_types) FROM report_model", nativeQuery = true)
+    Integer countTotalReports();
+
+    @Query(value = "SELECT COUNT(location_types) FROM report_model WHERE location_types = 'homepage'", nativeQuery = true)
+    Integer countTotalHomepage();
+
+    @Query(value = "SELECT COUNT(location_types) FROM report_model WHERE location_types = 'activitypage'", nativeQuery = true)
+    Integer countTotalActivitypage();
+
+    @Query(value = "SELECT COUNT(location_types) FROM report_model WHERE location_types = 'eventpage'", nativeQuery = true)
+    Integer countTotalEventpage();
+
+    @Query(value = "SELECT COUNT(location_types) FROM report_model WHERE location_types = 'matchinggame'", nativeQuery = true)
+    Integer countTotalMatchinggame();
+
+    @Query(value = "SELECT COUNT(location_types) FROM report_model WHERE location_types = 'hangmangame'", nativeQuery = true)
+    Integer countTotalHangmangame();
+
+    @Query(value = "SELECT COUNT(location_types) FROM report_model WHERE location_types = 'colorquizgame'", nativeQuery = true)
+    Integer countTotalColorquizgame();
+
+    @Query(value = "SELECT COUNT(location_types) FROM report_model WHERE location_types = 'animalgame'", nativeQuery = true)
+    Integer countTotalAnimalgame();
+
+    @Query(value = "SELECT COUNT(location_types) FROM report_model WHERE location_types = 'soundgame'", nativeQuery = true)
+    Integer countTotalSoundgame();
+
+    @Query(value = "SELECT COUNT(location_types) FROM report_model WHERE location_types = 'userportal'", nativeQuery = true)
+    Integer countTotalUserportal();
+
+    @Query(value = "SELECT COUNT(location_types) FROM report_model WHERE location_types = 'kidsportal'", nativeQuery = true)
+    Integer countTotalKidsportal();
 
 }
